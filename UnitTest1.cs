50925a5cbb02f5e0c44c0f470b0a0efd0cc3edf4
Michael Kirschner <mjk.kirschner@gmail.com>
2020-02-26 10:10:33 -0500

    add nullable

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -4,6 +4,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+#nullable enable
+
 namespace CsharpNullReferenceTypesCheckpoint
 {
     [TestClass]
@@ -14,20 +16,29 @@ namespace CsharpNullReferenceTypesCheckpoint
         {
             var student = new Student()
             {
-                FirstName = "John"
+                FirstName = "John",
+                LastName = "Smith",
+                Email = "someEmail",
+                Grade = 'A',
             };
 
             string result = ($"The student is called {student.FirstName}.");
 
             Assert.AreEqual("The student is called John.", result);
+            Assert.AreEqual(student.LastName, "Smith");
+            Assert.IsNotNull(student.Email);
+            Assert.IsNotNull(student.Grade);
+
         }
     }
 
     public class Student
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-        public char Grade { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public char? Grade { get; set; }
     }
 }
+
+#nullable disable
