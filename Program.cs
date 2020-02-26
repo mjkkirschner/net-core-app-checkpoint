da3442d5c3e81c5746c9b505e80da00843dceffd
Michael Kirschner <mjk.kirschner@gmail.com>
2020-02-26 12:46:37 -0500

    async commit

--- Program.cs
+++ Program.cs
@@ -1,39 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
-using Microsoft.Extensions.Configuration;
-using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace netcoreConfig
+namespace CsharpAsyncStreamsCheckpoint
 {
-
-    class Program
+    [TestClass]
+    public class TestsBefore
     {
-        static public string DefaultConnectionString { get; } = @"someConnectionString";
-        static IReadOnlyDictionary<string, string> DefaultConnectionStrings { get; } =
-        new Dictionary<string, string>()
+
+        [TestMethod]
+        public async Task Test1()
         {
-            {"profile:Username", Environment.UserName},
-            {"AppConfiguration:ConnectionString", DefaultConnectionString},
-            {"width", "50"}
-        };
-
-        static public IConfiguration Configuration { get; set; }
-
-        static void Main(string[] args)
-        {
-            args.ToList().ForEach(x => Console.WriteLine(x));
-            
-            ConfigurationBuilder builder = new ConfigurationBuilder();
-            builder.AddInMemoryCollection(DefaultConnectionStrings);
-            builder.AddJsonFile("./appsettings.json", false, true);
-            //add args to config.
-            builder.AddCommandLine(args);
-            Configuration = builder.Build();
-
-            //Console.SetWindowSize(Int32.Parse(Configuration["AppConfiguration:MainWindow:Width"]),Int32.Parse(Configuration["AppConfiguration:MainWindow:Height"]));
-            Console.WriteLine($"Hello {Configuration.GetValue<string>("h")}");
-            //debugging
-            //Configuration.AsEnumerable().ToList().ForEach(x => Console.WriteLine(x.Value));
+            /*
+            string result = "";
+            await foreach (var student in GetStudentsAsync())
+            {
+                result = result + ($"{student.FirstName} {student.LastName} - ");
+                Console.WriteLine(result);
+            }
+            Console.WriteLine("asdsadasdsad");
+            Assert.AreEqual("John Doe - Jane Doe - John Smith - ", result);
+            */
+              dostuff1();
+             await dostuff2();
         }
+
+        static async IAsyncEnumerable<Student> GetStudentsAsync()
+        {
+            await Task.Delay(2000);
+            var students = new List<Student>()
+            {
+                new Student() {FirstName = "John", LastName = "Doe", Email = "john.doe@gmail.com", Grade = 'A'},
+                new Student() {FirstName = "Jane", LastName = "Doe", Email="jane.doe@galvanize.com", Grade = 'B'},
+                new Student() {FirstName = "John", LastName = "Smith", Email = "john.smith@galvanize.com", Grade = 'C'}
+            };
+
+            foreach (var item in students)
+            {
+                yield return item;
+            }
+        }
+
+        static async Task dostuff1()
+        {
+            string result = "";
+            await foreach (var student in GetStudentsAsync())
+            {
+                result = result + ($"{student.FirstName} {student.LastName} - from dostuff 1");
+                Console.WriteLine(result);
+                 Console.WriteLine(System.Threading.Thread.CurrentThread.ManagedThreadId);
+            }
+        }
+        static async Task dostuff2()
+        {
+             string result = "";
+            await foreach (var student in GetStudentsAsync())
+            {
+                result = result + ($"{student.FirstName} {student.LastName} - from dostuff 2 ");
+                Console.WriteLine(result);
+                  Console.WriteLine(System.Threading.Thread.CurrentThread.ManagedThreadId);
+            }
+        }
+    }
+
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public char Grade { get; set; }
     }
 }
